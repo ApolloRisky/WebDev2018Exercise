c1c7f6fc7f3d46846ea0aa70a39e9a427e303026
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-04-05 22:04:57 +0700

    Update MultiplyTable

---
+++ Javascript Exercise/Multiplication table /js/multiplytable.js
@@ -0,0 +1,8 @@
+var GenerateResult = function(){
+    var nDOM = document.getElementById('nrow');
+    var mDOM = document.getElementById('ncol');
+    if(nDOM && mDOM){
+        var n = parseInt(nDOM.value);
+        var m = parseInt(mDOM.value);
+    }
+}
