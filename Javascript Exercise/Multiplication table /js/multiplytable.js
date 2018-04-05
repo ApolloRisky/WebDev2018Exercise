4fa1c83ee95c39e4a5de75eab8c01011271cabff
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-04-06 00:06:39 +0700

    Update slideShow

--- Javascript Exercise/Multiplication table /js/multiplytable.js
+++ Javascript Exercise/Multiplication table /js/multiplytable.js
@@ -1,8 +1,19 @@
+var mDOM = document.getElementById('nrow');
+var nDOM = document.getElementById('ncol');
+
 var GenerateResult = function(){
-    var nDOM = document.getElementById('nrow');
-    var mDOM = document.getElementById('ncol');
     if(nDOM && mDOM){
         var n = parseInt(nDOM.value);
         var m = parseInt(mDOM.value);
+      //  console.log(n);console.log(m); For DEBUG
+        var txt="";
+        for(var i=1;i<=n;i++){
+            txt+='<div class="table-item">'
+            for (var j=1;j<=m;j++){
+                txt+= i + ' x ' + j + ' = ' + i*j + '<br>';
+            }
+            txt+='</div>';
+        }
+        document.getElementById('result').innerHTML = txt;
     }
 }
--- Javascript Exercise/SlideShow/js/slide.js
+++ Javascript Exercise/SlideShow/js/slide.js
@@ -1,19 +1,36 @@
 var slideIndex = 0;
 showSlides();
+initThumbnails();
+function initThumbnails() {
+    var slides = document.getElementsByClassName("mySlides");
+    var thumbs = document.getElementsByClassName("thumb");
+    var slide_len = slides.length;
+    for(i=0;i<slide_len;i++){
+        var txt = "";
+        txt+='<img src="' + slides[i].childNodes[3].getAttribute('src') + '">';
+        thumbs[i].innerHTML = txt;
+    }
 
+}
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
+    var thumbs = document.getElementsByClassName('thumb');
     for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
+       slides[i].style.display = "none";
+       thumbs[i].style.border = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block";
+    thumbs[slideIndex-1].style.border = "3px solid blue";
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000);
+}
+var plusSlideIndex = function(n){
+    slideIndex += n;
 }
